34d27a8b6eefb891170056a1724cdaa0af21c1d0
tomaslin <clin@netflix.com>
2015-08-26 12:25:22 -0700

    fix spec for config controller now that we don't include the notification service there

--- app/scripts/modules/config/config.controller.spec.js
+++ app/scripts/modules/config/config.controller.spec.js
@@ -4,28 +4,21 @@ describe('Controller: Config', function () {
 
   var $controller;
   var configController;
-  var notificationService;
   var $rootScope;
   var $modal;
   var application;
 
   beforeEach(window.module(
-    require('./config.controller.js'),
-    require('./notification.service.js')
+    require('./config.controller.js')
   ));
 
   beforeEach(
-    window.inject(function (_$rootScope_, _$controller_, _$modal_, _notificationService_) {
+    window.inject(function (_$rootScope_, _$controller_, _$modal_) {
       $rootScope = _$rootScope_;
       $controller = _$controller_;
       $modal = _$modal_;
-      notificationService = _notificationService_;
     })
   );
-
-  beforeEach(function() {
-    spyOn(notificationService, 'getNotificationsForApplication').and.returnValue({then: angular.noop});
-  });
 
   describe('edit application ', function () {
     beforeEach( function() {
@@ -37,8 +30,7 @@ describe('Controller: Config', function () {
 
         configController = $controller('ConfigController', {
           app: application,
-          $modal: $modal,
-          notificationService: notificationService,
+          $modal: $modal
         });
       }
     );
